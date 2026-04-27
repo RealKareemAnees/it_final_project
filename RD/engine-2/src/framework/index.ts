1e57203280851bf4afff3c8b1ab53b6b11df1c30
Your Name <you@example.com>
2026-04-27 10:30:43 +0300

    removed legacy  code

--- RD/engine-2/src/framework/index.ts
+++
@@ -1,9 +0,0 @@
-// ============================================================
-//  MiniAct Framework – Barrel export
-//  Cleanly export all framework modules
-// ============================================================
-
-export * from "./types";
-export { Component } from "./component";
-export { h } from "./vdom";
-export { createDom, patchDOM, applyProps } from "./renderer";
