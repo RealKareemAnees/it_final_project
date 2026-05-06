fb989c97b7cc654eccefcf5a8752d8e2b26e4740
Your Name <you@example.com>
2026-05-06 09:07:03 +0300

    removed the backend

--- backend/src/db.ts
+++
@@ -1,5 +0,0 @@
-export { connectToDatabase, getDb, closeDatabase } from "./database";
-export * from "./repositories/users";
-export * from "./repositories/sessions";
-export * from "./repositories/cars";
-export * from "./repositories/feedback";
